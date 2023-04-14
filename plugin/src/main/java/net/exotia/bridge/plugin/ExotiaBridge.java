ef6047a142e4610c8b118a85ef5a7fde39b9f5aa
Wiktor Wilk <mcserw4@wp.pl>
2023-04-14 22:46:22 +0200

    setup maven modules

---
+++ plugin/src/main/java/net/exotia/bridge/plugin/ExotiaBridge.java
@@ -0,0 +1,17 @@
+package net.exotia.bridge.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExotiaBridge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
